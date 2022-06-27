4b4130758a1a7db1024373f1d66761d551e84a9a
Albin <albin.gottfridsson@hotmail.se>
2022-06-27 17:32:59 +0200

    WIP add buttons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+  // Countdown
   var toDate = new Date(2023, 0, 11, 11, 11);
   var toDateTime = 999;
   var daysLeft = 999;
@@ -7,14 +8,28 @@
   var secondsLeft = 999;
   var now = new Date().getTime();
 
+  // Video player
+  var videoElement;
+  var soundStateButton;
+  var videoStateButton;
+  var muteSymbol;
+  var unmuteSymbol;
+  var playSymbol;
+  var pauseSymbol;
+  var soundState;
+  var videoState;
+
 
   function init() {
+    // Init time
     toDateTime = toDate.getTime();
-    
     getTimeLeft();
     setInterval(() => {
       this.getTimeLeft();
     }, 1000);
+
+    // Init video player
+    initVideoControls();
   }
 
   function getTimeLeft() {
@@ -30,6 +45,63 @@
     document.querySelector('#hours').innerHTML = hoursLeft;
     document.querySelector('#minutes').innerHTML = minutesLeft;
     document.querySelector('#seconds').innerHTML = secondsLeft;
+  }
+
+  function initVideoControls() {
+    videoElement = document.querySelector('#video');
+    videoStateButton = document.querySelector('.video-state-button');
+    soundStateButton = document.querySelector('.sound-state-button');
+    muteSymbol = document.querySelector('.sound-state-button > .mute');
+    unmuteSymbol = document.querySelector('.sound-state-button > .unmute');
+    playSymbol = document.querySelector('.video-state-button > .play');
+    pauseSymbol = document.querySelector('.video-state-button > .pause');
+
+    // Hide mute and play
+    playSymbol.classList.add('d-none');
+    unmuteSymbol.classList.add('d-none');
+
+    soundState = "muted";
+    videoState = "playing";
+
+    soundStateButton.addEventListener('click', toggleSoundState);
+    videoStateButton.addEventListener('click', toggleVideoState);
+  }
+
+  function toggleSoundState() {
+    soundState = soundState === 'muted' ? 'unmuted' : 'muted';
+
+    console.log("toggling sound..." + soundState);
+
+    if(soundState === 'unmuted') {
+      videoElement.muted = false;
+      unmuteSymbol.classList.remove('d-none');
+      muteSymbol.classList.add('d-none');
+    } else if (soundState === 'muted') {
+      videoElement.muted = true;
+      unmuteSymbol.classList.add('d-none');
+      muteSymbol.classList.remove('d-none');
+    } else {
+      console.log("SHIIIIIT");
+    }
+  }
+
+  function toggleVideoState() {
+
+    videoState = videoState === 'playing' ? 'paused' : 'playing';
+
+    console.log("Toggling video to " + videoState);
+
+    if(videoState === 'playing') {
+      videoElement.play();
+      pauseSymbol.classList.remove('d-none');
+      playSymbol.classList.add('d-none');
+    } else if (videoState === 'paused') {
+      videoElement.pause();
+      pauseSymbol.classList.add('d-none');
+      playSymbol.classList.remove('d-none');
+    } else {
+      console.log("SHIIIIIT");
+    }
   }
 
   init();
